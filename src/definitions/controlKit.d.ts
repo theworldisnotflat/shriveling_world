78b6014d19e1ce125ec608642cf31a485240cf63
Farouk Abdou <kaktus40@hotmail.fr>
2019-03-04 21:35:50 +0100

    update

--- src/definitions/controlKit.d.ts
+++ src/definitions/controlKit.d.ts
@@ -81,6 +81,21 @@ declare module '@brunoimbrizi/controlkit' {
     step: number;
     dp: number;
   }
+  export interface IFunctionPlotter {
+    label?: string;
+  }
+  export interface IPadOptions {
+    label?: string;
+    height?: number;
+    resolution?: number;
+  }
+  export interface INumberOutputOptions {
+    label?: string;
+    dp: number;
+  }
+  export interface IStringOutputOptions {
+    label?: string;
+  }
   export class Component {
 
   }
@@ -90,15 +105,97 @@ declare module '@brunoimbrizi/controlkit' {
     public disable(): void;
     public addGroup(options?: IGroupOptions): Panel;
     public addSubGroup(options?: IGroupOptions): Panel;
+    /**
+     * [addStringInput description]
+     * @param  object   [description]
+     * @param  property the key to a string
+     * @param  options  [description]
+     * @return          [description]
+     */
     public addStringInput(object: any, property: string, options?: IStringOptions): Panel;
+    /**
+     * [addNumberInput description]
+     * @param  object   [description]
+     * @param  property the key to a number
+     * @param  options  [description]
+     * @return          [description]
+     */
     public addNumberInput(object: any, property: string, options?: INumberInputOptions): Panel;
+    /**
+     * [addRange description]
+     * @param  object   [description]
+     * @param  property the key to array of two number [min,max]
+     * @param  options  [description]
+     * @return          [description]
+     */
     public addRange(object: any, property: string, options?: IRangeOptions): Panel;
+    /**
+     * [addCheckbox description]
+     * @param  object   [description]
+     * @param  property the key to a boolean
+     * @param  options  [description]
+     * @return          [description]
+     */
     public addCheckbox(object: any, property: string, options?: ICheckboxOptions): Panel;
+    /**
+     * [addColor description]
+     * @param  object   [description]
+     * @param  property the key to a color matching colorMode option
+     * @param  options  [description]
+     * @return          [description]
+     */
     public addColor(object: any, property: string, options?: IColorOptions): Panel;
     public addButton(label: string, onPress: () => void, params?: { label?: string }): Panel;
+    /**
+     * [addSelect description]
+     * @param  object   [description]
+     * @param  property the key to an array of strings
+     * @param  options  [description]
+     * @return          [description]
+     */
     public addSelect(object: any, property: string, options?: ISelectOptions): Panel;
+    /**
+     * [addSlider description]
+     * @param  object   [description]
+     * @param  property the key to the number to evolve
+     * @param  range    the key to the range number to be used [min,max]
+     * @param  options  [description]
+     * @return          [description]
+     */
     public addSlider(object: any, property: string, range: string, options?: ISliderOptions): Panel;
-public addFunctionPlotter(object: any, property: string, options?: IStringOptions): Panel;
+    /**
+     * add a plotter
+     * @param  object   [description]
+     * @param  property the key to a function following this signature: (x?:number,y?:number)=>number
+     * @param  options  [description]
+     * @return          [description]
+     */
+    public addFunctionPlotter(object: any, property: string, options?: IFunctionPlotter): Panel;
+    /**
+     * [addPad description]
+     * @param  object   [description]
+     * @param  property key to the position (array of two numbers [x,y])
+     * @param  options  [description]
+     * @return          [description]
+     */
+    public addPad(object: any, property: string, options?: IPadOptions): Panel;
+    /**
+     * [addValuePlotter description]
+     * @param  object   [description]
+     * @param  property key to a number value
+     * @param  options  [description]
+     * @return          [description]
+     */
+    public addValuePlotter(object: any, property: string, options?: IPadOptions): Panel;
+    /**
+     * [addNumberOutput description]
+     * @param  object   [description]
+     * @param  property key to a number value
+     * @param  options  [description]
+     * @return          [description]
+     */
+    public addNumberOutput(object: any, property: string, options?: INumberOutputOptions): Panel;
+    public addStringOutput(object: any, property: string, options?: IStringOutputOptions): Panel;
   }
   export type nodeType = 'div' | 'text' | 'button' | 'select' | 'checkbox' | 'option' | 'ul' | 'li' | 'span' | 'textarea';
   export class Node {
