12b8849103179e015e2175a5c782220589c71f3a
Abdou <kaktus40@hotmail.fr>
2020-11-13 13:03:22 +0100

    fdfd

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,9 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
+const url = dev ? '/' : 'shriveling_world';
 polka() // You can also use Express
-	.use(compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
+	.use(url, compression({ threshold: 0 }), sirv('static', { dev }), sapper.middleware())
 	.listen(PORT, (err) => {
 		if (err) console.log('error', err);
 	});
